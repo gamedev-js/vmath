f13211ff8a1b6a75718ce10547c4db2108918459
Johnny Wu <KarasAya@gmail.com>
2017-04-12 11:13:20 +0800

    bump to 1.2.0

--- dist/vmath.js
+++ dist/vmath.js
@@ -1,6 +1,6 @@
 
 /*
- * vmath v1.1.3
+ * vmath v1.2.0
  * (c) 2017 @Johnny Wu
  * Released under the MIT License.
  */
@@ -68,17 +68,6 @@ function clamp01(val) {
 }
 
 /**
- * Returns true if argument is a power-of-two and false otherwise.
- *
- * @method powof2
- * @param {number} x Number to check for power-of-two property.
- * @returns {boolean} true if power-of-two and false otherwise.
- */
-function powof2(x) {
-  return ((x !== 0) && !(x & (x - 1)));
-}
-
-/**
  * @method lerp
  * @param {number} from
  * @param {number} to
@@ -135,6 +124,324 @@ function randomRange(min, max) {
 function randomRangeInt(min, max) {
   return Math.floor(this.randomRange(min, max));
 }
+
+/**
+ * Bit twiddling hacks for JavaScript.
+ *
+ * Author: Mikola Lysenko
+ *
+ * Ported from Stanford bit twiddling hack library:
+ *    http://graphics.stanford.edu/~seander/bithacks.html
+ */
+
+// Number of bits in an integer
+const INT_BITS = 32;
+const INT_MAX =  0x7fffffff;
+const INT_MIN = -1<<(INT_BITS-1);
+
+/**
+ * Returns -1, 0, +1 depending on sign of x
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function sign(v) {
+  return (v > 0) - (v < 0);
+}
+
+/**
+ * Computes absolute value of integer
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function abs(v) {
+  let mask = v >> (INT_BITS-1);
+  return (v ^ mask) - mask;
+}
+
+/**
+ * Computes minimum of integers x and y
+ *
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+function min(x, y) {
+  return y ^ ((x ^ y) & -(x < y));
+}
+
+/**
+ * Computes maximum of integers x and y
+ *
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+function max(x, y) {
+  return x ^ ((x ^ y) & -(x < y));
+}
+
+/**
+ * Checks if a number is a power of two
+ *
+ * @param {number} v
+ * @returns {boolean}
+ */
+function isPow2(v) {
+  return !(v & (v-1)) && (!!v);
+}
+
+/**
+ * Computes log base 2 of v
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function log2(v) {
+  let r, shift;
+  r =     (v > 0xFFFF) << 4; v >>>= r;
+  shift = (v > 0xFF  ) << 3; v >>>= shift; r |= shift;
+  shift = (v > 0xF   ) << 2; v >>>= shift; r |= shift;
+  shift = (v > 0x3   ) << 1; v >>>= shift; r |= shift;
+  return r | (v >> 1);
+}
+
+/**
+ * Computes log base 10 of v
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function log10(v) {
+  return  (v >= 1000000000) ? 9 : (v >= 100000000) ? 8 : (v >= 10000000) ? 7 :
+          (v >= 1000000) ? 6 : (v >= 100000) ? 5 : (v >= 10000) ? 4 :
+          (v >= 1000) ? 3 : (v >= 100) ? 2 : (v >= 10) ? 1 : 0;
+}
+
+/**
+ * Counts number of bits
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function popCount(v) {
+  v = v - ((v >>> 1) & 0x55555555);
+  v = (v & 0x33333333) + ((v >>> 2) & 0x33333333);
+  return ((v + (v >>> 4) & 0xF0F0F0F) * 0x1010101) >>> 24;
+}
+
+/**
+ * Counts number of trailing zeros
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function countTrailingZeros(v) {
+  let c = 32;
+  v &= -v;
+  if (v) c--;
+  if (v & 0x0000FFFF) c -= 16;
+  if (v & 0x00FF00FF) c -= 8;
+  if (v & 0x0F0F0F0F) c -= 4;
+  if (v & 0x33333333) c -= 2;
+  if (v & 0x55555555) c -= 1;
+  return c;
+}
+
+/**
+ * Rounds to next power of 2
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function nextPow2(v) {
+  v += v === 0;
+  --v;
+  v |= v >>> 1;
+  v |= v >>> 2;
+  v |= v >>> 4;
+  v |= v >>> 8;
+  v |= v >>> 16;
+  return v + 1;
+}
+
+/**
+ * Rounds down to previous power of 2
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function prevPow2(v) {
+  v |= v >>> 1;
+  v |= v >>> 2;
+  v |= v >>> 4;
+  v |= v >>> 8;
+  v |= v >>> 16;
+  return v - (v>>>1);
+}
+
+/**
+ * Computes parity of word
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function parity(v) {
+  v ^= v >>> 16;
+  v ^= v >>> 8;
+  v ^= v >>> 4;
+  v &= 0xf;
+  return (0x6996 >>> v) & 1;
+}
+
+const REVERSE_TABLE = new Array(256);
+
+(function(tab) {
+  for(let i=0; i<256; ++i) {
+    let v = i, r = i, s = 7;
+    for (v >>>= 1; v; v >>>= 1) {
+      r <<= 1;
+      r |= v & 1;
+      --s;
+    }
+    tab[i] = (r << s) & 0xff;
+  }
+})(REVERSE_TABLE);
+
+/**
+ * Reverse bits in a 32 bit word
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function reverse(v) {
+  return (REVERSE_TABLE[v & 0xff] << 24) |
+         (REVERSE_TABLE[(v >>> 8) & 0xff] << 16) |
+         (REVERSE_TABLE[(v >>> 16) & 0xff] << 8) |
+         REVERSE_TABLE[(v >>> 24) & 0xff];
+}
+
+/**
+ * Interleave bits of 2 coordinates with 16 bits. Useful for fast quadtree codes
+ *
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+function interleave2(x, y) {
+  x &= 0xFFFF;
+  x = (x | (x << 8)) & 0x00FF00FF;
+  x = (x | (x << 4)) & 0x0F0F0F0F;
+  x = (x | (x << 2)) & 0x33333333;
+  x = (x | (x << 1)) & 0x55555555;
+
+  y &= 0xFFFF;
+  y = (y | (y << 8)) & 0x00FF00FF;
+  y = (y | (y << 4)) & 0x0F0F0F0F;
+  y = (y | (y << 2)) & 0x33333333;
+  y = (y | (y << 1)) & 0x55555555;
+
+  return x | (y << 1);
+}
+
+/**
+ * Extracts the nth interleaved component
+ *
+ * @param {number} v
+ * @param {number} n
+ * @returns {number}
+ */
+function deinterleave2(v, n) {
+  v = (v >>> n) & 0x55555555;
+  v = (v | (v >>> 1))  & 0x33333333;
+  v = (v | (v >>> 2))  & 0x0F0F0F0F;
+  v = (v | (v >>> 4))  & 0x00FF00FF;
+  v = (v | (v >>> 16)) & 0x000FFFF;
+  return (v << 16) >> 16;
+}
+
+/**
+ * Interleave bits of 3 coordinates, each with 10 bits.  Useful for fast octree codes
+ *
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @returns {number}
+ */
+function interleave3(x, y, z) {
+  x &= 0x3FF;
+  x  = (x | (x<<16)) & 4278190335;
+  x  = (x | (x<<8))  & 251719695;
+  x  = (x | (x<<4))  & 3272356035;
+  x  = (x | (x<<2))  & 1227133513;
+
+  y &= 0x3FF;
+  y  = (y | (y<<16)) & 4278190335;
+  y  = (y | (y<<8))  & 251719695;
+  y  = (y | (y<<4))  & 3272356035;
+  y  = (y | (y<<2))  & 1227133513;
+  x |= (y << 1);
+
+  z &= 0x3FF;
+  z  = (z | (z<<16)) & 4278190335;
+  z  = (z | (z<<8))  & 251719695;
+  z  = (z | (z<<4))  & 3272356035;
+  z  = (z | (z<<2))  & 1227133513;
+
+  return x | (z << 2);
+}
+
+/**
+ * Extracts nth interleaved component of a 3-tuple
+ *
+ * @param {number} v
+ * @param {number} n
+ * @returns {number}
+ */
+function deinterleave3(v, n) {
+  v = (v >>> n)       & 1227133513;
+  v = (v | (v>>>2))   & 3272356035;
+  v = (v | (v>>>4))   & 251719695;
+  v = (v | (v>>>8))   & 4278190335;
+  v = (v | (v>>>16))  & 0x3FF;
+  return (v<<22)>>22;
+}
+
+/**
+ * Computes next combination in colexicographic order (this is mistakenly called nextPermutation on the bit twiddling hacks page)
+ *
+ * @param {number} v
+ * @returns {number}
+ */
+function nextCombination(v) {
+  let t = v | (v - 1);
+  return (t + 1) | (((~t & -~t) - 1) >>> (countTrailingZeros(v) + 1));
+}
+
+var bits_ = Object.freeze({
+	INT_BITS: INT_BITS,
+	INT_MAX: INT_MAX,
+	INT_MIN: INT_MIN,
+	sign: sign,
+	abs: abs,
+	min: min,
+	max: max,
+	isPow2: isPow2,
+	log2: log2,
+	log10: log10,
+	popCount: popCount,
+	countTrailingZeros: countTrailingZeros,
+	nextPow2: nextPow2,
+	prevPow2: prevPow2,
+	parity: parity,
+	reverse: reverse,
+	interleave2: interleave2,
+	deinterleave2: deinterleave2,
+	interleave3: interleave3,
+	deinterleave3: deinterleave3,
+	nextCombination: nextCombination
+});
 
 class _vec2 {
   constructor(x, y) {
@@ -6193,6 +6500,10 @@ mat4.equals = function (a, b) {
   );
 };
 
+// NOTE: there is no syntax for: export {* as bits} from './lib/bits';
+let bits = bits_;
+
+exports.bits = bits;
 exports.vec2 = vec2;
 exports.vec3 = vec3;
 exports.vec4 = vec4;
@@ -6206,7 +6517,6 @@ exports.equals = equals;
 exports.approx = approx;
 exports.clamp = clamp;
 exports.clamp01 = clamp01;
-exports.powof2 = powof2;
 exports.lerp = lerp;
 exports.toRadian = toRadian;
 exports.toDegree = toDegree;
