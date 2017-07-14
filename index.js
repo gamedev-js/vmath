9b802b664245fb47f30bb92674a8eea564585589
Johnny Wu <KarasAya@gmail.com>
2017-07-14 16:53:13 +0800

    for better export

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-export * from './lib/utils';
+import * as utils from './lib/utils';
 
 // NOTE: there is no syntax for: export {* as bits} from './lib/bits';
-import * as bits_ from './lib/bits';
-export let bits = bits_;
+import * as bits from './lib/bits';
+import vec2 from './lib/vec2';
+import vec3 from './lib/vec3';
+import vec4 from './lib/vec4';
+import quat from './lib/quat';
+import mat2 from './lib/mat2';
+import mat23 from './lib/mat23';
+import mat3 from './lib/mat3';
+import mat4 from './lib/mat4';
+import color3 from './lib/color3';
+import color4 from './lib/color4';
 
-export { default as vec2 } from './lib/vec2';
-export { default as vec3 } from './lib/vec3';
-export { default as vec4 } from './lib/vec4';
-export { default as quat } from './lib/quat';
-export { default as mat2 } from './lib/mat2';
-export { default as mat23 } from './lib/mat23';
-export { default as mat3 } from './lib/mat3';
-export { default as mat4 } from './lib/mat4';
-export { default as color3 } from './lib/color3';
-export { default as color4 } from './lib/color4';
+let vmath = {
+  bits,
+  vec2,
+  vec3,
+  vec4,
+  quat,
+  mat2,
+  mat23,
+  mat3,
+  mat4,
+  color3,
+  color4,
+};
+Object.assign(vmath, utils);
+
+export default vmath;
